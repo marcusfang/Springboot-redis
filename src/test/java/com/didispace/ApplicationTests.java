69e8ad220493c4f3d7942fe6c87f49f28e58c9d6
marcusfang <marcusfang@163.com>
2019-04-01 16:10:15 +0800

    init

--- src/test/java/com/didispace/ApplicationTests.java
+++ src/test/java/com/didispace/ApplicationTests.java
@@ -4,16 +4,15 @@ import com.didispace.domain.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import javax.annotation.Resource;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {Application.class})
+@SpringBootTest(classes =Application.class)
 public class ApplicationTests {
 
 	@Resource
